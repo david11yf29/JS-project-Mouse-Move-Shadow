96d6e074f30a8a87f09dd3d72a62b900fac0adcb
david11yf29 <david11yf29@gmail.com>
2020-03-16 21:32:59 +0800

    updated

--- scripts.js
+++ scripts.js
@@ -1,4 +1,22 @@
-const elf = {
-    name: 'Orwell',
-    weapon: 'bow'
+// const elf = {
+//     name: 'Orwell',
+//     weapon: 'bow',
+//     attack() {
+//         return 'attach with ' + elf.weapon
+//     }
+// }
+
+// console.log(elf.attack());
+
+function createElf(name, weapon) {
+    return {
+        name: name,
+        weapon: weapon,
+        attack() {
+            return 'attack with ' + weapon
+        }
+    }
 }
+
+const peter = createElf('Peter', 'stones');
+console.log(peter.attack());
