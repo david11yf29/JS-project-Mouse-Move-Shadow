64d8f1f1d2d5daa62651d2068f95b91f407b1809
david11yf29 <david11yf29@gmail.com>
2020-03-21 22:07:34 +0800

    updated

--- scripts.js
+++ scripts.js
@@ -3,14 +3,14 @@
     var boo = 1;
     try {
       throw new Error('some example');
-    } catch (boo) {
+    } catch (err) {
       var err = 3;
       var boo = 10;
-      console.log(boo); // 10
       console.log(err); // 3
       console.log(boo); // 10
     }
+
     //Guess what the output is here:
-    console.log(err); // 3
-    console.log(boo); // 1
+    console.log(err); // 5
+    console.log(boo); // 10
   })();
